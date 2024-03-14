516e7ba00b9a51a07d978e8882905db3859335c4
wokaotianshi123 <56501836+wokaotianshi123@users.noreply.github.com>
2024-03-14 11:29:03 +0800

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,13 @@
 export const proxy = [
-  'https://diii.tk',
-  'https://api-pikpak.tjsky.cf',
-  'https://api-pikpak.tjsky-1.cf',
-  'https://cf-api.xswd.cf',
-  'https://wkr.btstream.net',
-  'https://pikpak.520936.xyz',
-  'https://wkr.btstream.net',
-  'https://darazemon.xyz',
+  'https://pik.tianshideyou.cloudns.org',
+ // 'https://diii.tk',
+ // 'https://api-pikpak.tjsky.cf',
+ // 'https://api-pikpak.tjsky-1.cf',
+ // 'https://cf-api.xswd.cf',
+ // 'https://wkr.btstream.net',
+ // 'https://pikpak.520936.xyz',
+ // 'https://wkr.btstream.net',
+ // 'https://darazemon.xyz',
   'https://pikpak.995579.xyz',
 ]
 
